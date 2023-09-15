28469ea6abf981b804ffbf9134cc192398b2a027
Dannysnotepad <etimdnl41@gmail.com>
2023-09-15 09:58:20 +0100

    🌟0.3

--- main.js
+++ main.js
@@ -31,24 +31,30 @@ form.addEventListener('submit', (e) => {
   e.preventDefault()
   
   let input = userInput.value
-  let url = `https://api.dictionaryapi.dev/api/v2/entries/en/${input}`
-  let response
-  
-  let wordSearch = new XMLHttpRequest()
-  wordSearch.open('GET', url)
-  wordSearch.send()
-  
-  wordSearch.onload = () => {
-    if (wordSearch.status === 200) {
-      response = JSON.parse(wordSearch.response)
-      console.log(response[0].meanings[0].definitions[0].definition)
-      searchResult.textContent = `${response[0].meanings[0].definitions[0].definition}`
-    } else if(wordSearch.status === 404){
-      response = JSON.parse(wordSearch.response)
-      console.log(response.title)
-      searchResult.textContent = `${response.title}`
+  if(input == '' || input == null || !isNaN(input)) {
+    alert('Please enter a corrct and appropriate word')
+  }else{
+    
+    let url = `https://api.dictionaryapi.dev/api/v2/entries/en/${input}`
+    let response
+    
+    let wordSearch = new XMLHttpRequest()
+    wordSearch.open('GET', url)
+    wordSearch.send()
+    
+    wordSearch.onload = () => {
+      if (wordSearch.status === 200) {
+        response = JSON.parse(wordSearch.response)
+        console.log(response[0].meanings[0].definitions[0].definition)
+        searchResult.textContent = `${response[0].meanings[0].definitions[0].definition}`
+      } else if (wordSearch.status === 404) {
+        response = JSON.parse(wordSearch.response)
+        console.log(response.title)
+        searchResult.textContent = `${response.title}`
+      }
     }
+    wordSearch.onload()
+    
   }
-  wordSearch.onload()
     
 })
